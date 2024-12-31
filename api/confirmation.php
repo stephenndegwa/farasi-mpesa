8befd843196fa1e3a55be79e0b64954b1a3a1b9d
Stephen Ndegwa <105418748+stephenndegwa@users.noreply.github.com>
2024-12-31 13:03:55 +0300

    Updated saveTransaction function to include MiddleName and LastName fields in transactions table. Adjusted SQL query and parameters for compatibility.

--- api/confirmation.php
+++ api/confirmation.php
@@ -5,11 +5,53 @@ function saveTransaction($transaction)
 {
     $pdo = getDbConnection();
 
-    $sql = "INSERT INTO transactions (TransactionType, TransID, TransTime, TransAmount, BusinessShortCode, BillRefNumber, InvoiceNumber, OrgAccountBalance, ThirdPartyTransID, MSISDN, FirstName) 
-            VALUES (:TransactionType, :TransID, :TransTime, :TransAmount, :BusinessShortCode, :BillRefNumber, :InvoiceNumber, :OrgAccountBalance, :ThirdPartyTransID, :MSISDN, :FirstName)";
+    $sql = "INSERT INTO transactions (
+                TransactionType, 
+                TransID, 
+                TransTime, 
+                TransAmount, 
+                BusinessShortCode, 
+                BillRefNumber, 
+                InvoiceNumber, 
+                OrgAccountBalance, 
+                ThirdPartyTransID, 
+                MSISDN, 
+                FirstName, 
+                MiddleName, 
+                LastName
+            ) 
+            VALUES (
+                :TransactionType, 
+                :TransID, 
+                :TransTime, 
+                :TransAmount, 
+                :BusinessShortCode, 
+                :BillRefNumber, 
+                :InvoiceNumber, 
+                :OrgAccountBalance, 
+                :ThirdPartyTransID, 
+                :MSISDN, 
+                :FirstName, 
+                :MiddleName, 
+                :LastName
+            )";
 
     $stmt = $pdo->prepare($sql);
-    $stmt->execute($transaction);
+    $stmt->execute([
+        ':TransactionType' => $transaction['TransactionType'] ?? null,
+        ':TransID' => $transaction['TransID'] ?? null,
+        ':TransTime' => $transaction['TransTime'] ?? null,
+        ':TransAmount' => $transaction['TransAmount'] ?? null,
+        ':BusinessShortCode' => $transaction['BusinessShortCode'] ?? null,
+        ':BillRefNumber' => $transaction['BillRefNumber'] ?? null,
+        ':InvoiceNumber' => $transaction['InvoiceNumber'] ?? null,
+        ':OrgAccountBalance' => $transaction['OrgAccountBalance'] ?? null,
+        ':ThirdPartyTransID' => $transaction['ThirdPartyTransID'] ?? null,
+        ':MSISDN' => $transaction['MSISDN'] ?? null,
+        ':FirstName' => $transaction['FirstName'] ?? null,
+        ':MiddleName' => $transaction['MiddleName'] ?? null,
+        ':LastName' => $transaction['LastName'] ?? null
+    ]);
 }
 
 // function logData($data, $file = 'transaction_logs.txt')
